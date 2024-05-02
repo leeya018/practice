1049d3f7ed49b23c2203b4b3e478c04da00c8527
leeya018 <leeyahav018@gmail.com>
2024-05-02 15:34:32 +0300

    useEffcet explained

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,44 @@
-"use cilent";
-import React from "react";
-import Fibonacci from "@/components/Fibonacci";
+"use client";
+import React, { useState, useEffect } from "react";
 
-// 1 2 3 5 8
 export default function page() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  // useEffect(() => {
+  //   // fect function
+  //   const getData = async () => {
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  //     const data = await res.json();
+  //     console.log(data);
+  //     return data;
+  //   };
+  //   getData();
+  // }, []);
+
+  // useEffect(() => {
+  //   const resizeFunc = () => console.log(window.innerWidth);
+  //   window.addEventListener("resize", resizeFunc);
+
+  //   return () => {
+  //     window.removeEventListener("resize", resizeFunc);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    console.log({ name, count });
+  }, [count, name]);
+
+  // console.log("component");
   return (
     <div>
-      <Fibonacci num1={1} num2={3} />
+      useEffect
+      <button onClick={() => setCount(count + 1)}>increase counter</button>
+      <input
+        className="border-2 border-black"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </div>
   );
 }
